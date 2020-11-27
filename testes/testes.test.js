8c205751ae752efff66d05a182cd421c09f3f933
Guilherme Andrade <gui-keanne@hotmail.com>
2020-11-27 14:25:01 -0300

    deixando codigo dos testes mais descritivo

--- testes/testes.test.js
+++ testes/testes.test.js
@@ -1,32 +1,85 @@
 const testes = require('../funcoes/testes');
 
-test('Testar validade semantica do CPF', () => {
-  expect(testes.cpf('')).toBe(false);
-  expect(testes.cpf('a256f8965ff')).toBe(false);
-  expect(testes.cpf('1256589658')).toBe(false);
-  expect(testes.cpf('125632565689')).toBe(false);
-  expect(testes.cpf('55896589658')).toBe(false);
-  expect(testes.cpf(0)).toBe(false);
-  expect(testes.cpf(37885788822)).toBe(false);
-  expect(testes.cpf('37885788822')).toBe(true);
+describe('Testar validade semântica do CPF', () => {
+  it('valor nulo', () => {
+    expect(testes.cpf('')).toBe(false);
+  });
+
+  it('valores alfanuméricos (letras e números)', () => {
+    expect(testes.cpf('a256f8965ff')).toBe(false);
+  });
+
+  it('dez caracteres numéricos', () => {
+    expect(testes.cpf('1256589658')).toBe(false);
+  });
+
+  it('doze caracteres numéricos', () => {
+    expect(testes.cpf('125632565689')).toBe(false);
+  });
+
+  it('CPF numérico inválido', () => {
+    expect(testes.cpf('55896589658')).toBe(false);
+  });
+
+  it('CPF com tipagem diferente de String', () => {
+    expect(testes.cpf(37885788822)).toBe(false);
+  });
+
+  it('CPF válido', () => {
+    expect(testes.cpf('37885788822')).toBe(true);
+  });
 });
 
-test('Testar validade semantica do Email', () => {
-  expect(testes.email('')).toBe(false);
-  expect(testes.email(0)).toBe(false);
-  expect(testes.email('email@email.com')).toBe(true);
-  expect(testes.email('email.com')).toBe(false);
+describe('Testar validade semântica do E-mail', () => {
+  it('E-mail com valor nulo', () => {
+    expect(testes.email('')).toBe(false);
+  });
+
+  it('E-mail com valor com tipagem diferente de String', () => {
+    expect(testes.email(0)).toBe(false);
+  });
+
+  it('E-mail inválido', () => {
+    expect(testes.email('email.com')).toBe(false);
+  });
+
+  it('E-mail válido', () => {
+    expect(testes.email('email@email.com')).toBe(true);
+  });
 });
 
-test('Testar se data está no formato brasileiro separado por barras "/"', () => {
-  expect(testes.dataBrl(0)).toBe(false);
-  expect(testes.dataBrl('')).toBe(false);
-  expect(testes.dataBrl('25/10/202r')).toBe(false);
-  expect(testes.dataBrl('2020/05/01')).toBe(false);
-  expect(testes.dataBrl('1989-25-02')).toBe(false);
-  expect(testes.dataBrl('30/02/2020')).toBe(false);
-  expect(testes.dataBrl('28-02-2020')).toBe(false);
-  expect(testes.dataBrl('28/02/2020')).toBe(true);
+describe('Testar se data está no formato brasileiro separado por barras "/"', () => {
+  it('Data com valor numérico zero', () => {
+    expect(testes.dataBrl(0)).toBe(false);
+  });
+
+  it('Data com valor nulo', () => {
+    expect(testes.dataBrl('')).toBe(false);
+  });
+
+  it('Data inválida', () => {
+    expect(testes.dataBrl('25/10/202r')).toBe(false);
+  });
+
+  it('Data correta em formato SQL (aaaa/mm/dd)', () => {
+    expect(testes.dataBrl('2020/05/01')).toBe(false);
+  });
+
+  it('Data inválida separada por traços ( - )', () => {
+    expect(testes.dataBrl('1989-25-02')).toBe(false);
+  });
+
+  it('Data com a máscara correta porém inexistente', () => {
+    expect(testes.dataBrl('30/02/2020')).toBe(false);
+  });
+
+  it('Data com formato BRL porém separada por traços', () => {
+    expect(testes.dataBrl('28-02-2020')).toBe(false);
+  });
+
+  it('Data válida no formato BRL', () => {
+    expect(testes.dataBrl('28/02/2020')).toBe(true);
+  });
 });
 
 /* REGRAS DA SENHA */
@@ -38,18 +91,41 @@ test('Testar se data está no formato brasileiro separado por barras "/"', () =>
  * 5 - Não pode conter qualquer outro caracter especial que
  *     não seja o asterisco
  */
-test('Verificar se a senha está de acordo com as regras', () => {
-  expect(testes.senha('aA2*ppppp')).toBe(false);
-  expect(testes.senha('aa2*pppppp')).toBe(false);
-  expect(testes.senha('aAa*pppppp')).toBe(false);
-  expect(testes.senha('aA2apppppp')).toBe(false);
-  expect(testes.senha('*A2a/ppppp')).toBe(false);
-  expect(testes.senha('aA2*ppppppppppppppppp')).toBe(false);
-  expect(testes.senha('aA2*pppppp')).toBe(true);
+
+describe('Verificar se senha está de acordo com as regras', () => {
+  it('Senha com menos de 10 caracteres', () => {
+    expect(testes.senha('aA2*ppppp')).toBe(false);
+  });
+
+  it('Senha com mais de 10 caracteres', () => {
+    expect(testes.senha('aA2*ppppppppppppppppp')).toBe(false);
+  });
+
+  it('Senha sem uma letra maiúscula', () => {
+    expect(testes.senha('aa2*pppppp')).toBe(false);
+  });
+
+  it('Senha sem números', () => {
+    expect(testes.senha('aAa*pppppp')).toBe(false);
+  });
+
+  it('Senha sem asterisco', () => {
+    expect(testes.senha('aA2apppppp')).toBe(false);
+  });
+
+  it('Senha com caracteres especiais além do asterisco', () => {
+    expect(testes.senha('*A2a/ppppp')).toBe(false);
+  });
+
+  it('Senha válida', () => {
+    expect(testes.senha('aA2*pppppp')).toBe(true);
+  });
 });
 
-test('exemplo de mock', () => {
-  const terceiro = jest.fn();
-  terceiro.mockReturnValue(1);
-  expect(testes.funcaoTerceira(10, terceiro)).toBe(11);
+describe('Exemplo de um teste com mock para demonstração', () => {
+  it('Teste mock', () => {
+    const terceiro = jest.fn();
+    terceiro.mockReturnValue(1);
+    expect(testes.funcaoTerceira(10, terceiro)).toBe(11);
+  });
 });
